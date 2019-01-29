2abbf6024ad34a96df1356470e6abdf6bb87ca7f
Tamas Puskas <tamasp1993@live.com>
2019-01-29 12:43:00 +0100

    File not found exception added

---
+++ week3/day_1/src/com/company/PrintEachLine.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.io.IOException;
+
+import java.nio.file.Path;
+
+import java.nio.file.Paths;
+
+import java.nio.file.Files;
+
+import java.util.List;
+
+public class PrintEachLine {
+        public static void main(String[] args) {
+            try {
+                Path filePath = Paths.get("my-file.txt");
+                List<String> themLines = Files.readAllLines(filePath);
+            } catch (IOException ex){
+                System.out.println("Unable to read file: my-file.txt");
+            }
+
+        }
+    }
+
