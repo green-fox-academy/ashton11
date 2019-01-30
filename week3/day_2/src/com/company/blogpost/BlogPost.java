20c732e4f230945fced1245ecfdbf7fbaba88de2
Tamas Puskas <tamasp1993@live.com>
2019-01-30 12:46:17 +0100

    Blogs Posted

--- week3/day_2/src/com/company/blogpost/BlogPost.java
+++ week3/day_2/src/com/company/blogpost/BlogPost.java
@@ -1,5 +1,18 @@
 package com.company.blogpost;
 
 public class BlogPost {
-    
+    String title;
+    String authorName;
+    String publicationDate;
+    String text;
+    public BlogPost (String title, String authorName, String publicationDate, String text ) {
+        this.title = title + "\n";
+        this.authorName = "Author of post: " + authorName ;
+        this.publicationDate = "\n" + publicationDate;
+        this.text = "\n" + text + "\n";
+    }
+    public void writeBlogPost (){
+        System.out.println( title + authorName + publicationDate + text);
+    }
 }
+
