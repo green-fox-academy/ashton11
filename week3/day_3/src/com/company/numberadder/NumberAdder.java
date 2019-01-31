4f26a36feeb5ccf8fef4152f1b432b24dcba53dd
Tamas Puskas <tamasp1993@live.com>
2019-01-31 11:29:43 +0100

    Finished with NumberAdder

--- week3/day_3/src/com/company/numberadder/NumberAdder.java
+++ week3/day_3/src/com/company/numberadder/NumberAdder.java
@@ -1,4 +1,16 @@
 package com.company.numberadder;
 
 public class NumberAdder {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(numberAdder(n));
+    }
+
+public static int numberAdder(int n) {
+        int added = 0;
+        if (n > 0) {
+            added = n + numberAdder(n-1);
+            }
+    return added;
+    }
 }
