a67fa4adf78bdd47f192768a1ddde1dd8b80ea4d
Tamas Puskas <tamasp1993@live.com>
2019-02-05 19:31:29 +0100

    Created make function of Aircrafts

--- week4/day_1/src/aircraftcarrier/Aircrafts.java
+++ week4/day_1/src/aircraftcarrier/Aircrafts.java
@@ -1,4 +1,18 @@
 package aircraftcarrier;
 
 public class Aircrafts {
+   String type ;
+
+
+    private Aircrafts(String type){
+        this.type= type;
+    }
+
+    public static Aircrafts make(String type) {
+        if(!type.equals("f35") && !type.equals("f16")) {
+            return null;
+        }
+        return new Aircrafts(type);
+    }
+
 }
--- week4/day_1/src/aircraftcarrier/Main.java
+++ week4/day_1/src/aircraftcarrier/Main.java
@@ -1,4 +1,11 @@
 package aircraftcarrier;
 
 public class Main {
+    public static void main(String[] args) {
+        Aircrafts aircraft = Aircrafts.make("f35");
+        System.out.println(aircraft.type);
+        aircraft.type = "ez lett a tipusa";
+
+        System.out.println(aircraft.type);
+    }
 }
