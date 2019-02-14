a84a98da4567d5b7b0d0b0f3c3172f60657222cd
Tamas Puskas <tamasp1993@live.com>
2019-02-14 09:54:09 +0100

    Done with SwapNew and added Unique

--- week2/day_1/src/com/company/SwapNew.java
+++ week2/day_1/src/com/company/SwapNew.java
@@ -1,19 +1,16 @@
 package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 // - Create an array variable named `abc`
 //   with the following content: `["first", "second", "third"]`
 // - Swap the first and the third element of `abc`
 public class SwapNew {
     public static void main(String[] args) {
-        String abc[] = {"first", "second", "third"};
-        String mid = "";
-        for (int i = 0; i < abc.length ; i++) {
+        String[] abc = {"first", "second", "third"};
+        Collections.swap(Arrays.asList(abc),0 ,2);
 
-        if (i == 0){
-        abc[0] = mid;
-        abc
-        }
-
-        }
         System.out.println(abc);
     }
 }
