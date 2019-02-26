fc9453786d3f33c8ad29d2137501dfd06138034c
Tamas Puskas <tamasp1993@live.com>
2019-02-26 12:29:56 +0100

    Grerting counter added to html file too via WebController

--- orientation/week1/day2/src/main/java/com/greenfox/springstart/controllers/HelloWebController.java
+++ orientation/week1/day2/src/main/java/com/greenfox/springstart/controllers/HelloWebController.java
@@ -1,4 +1,18 @@
 package com.greenfox.springstart.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
 public class HelloWebController {
+AtomicLong counter2 = new AtomicLong(1);
+    @RequestMapping("/web/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("name", " World");
+        model.addAttribute("counter", "This site was loaded " + counter2.getAndAdd(1) + " times since last server start" );
+        return "greeting";
+    }
 }
