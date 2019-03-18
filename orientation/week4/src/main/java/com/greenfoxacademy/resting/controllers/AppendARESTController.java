5380fdd3348d730f6a5d7a76133dd39125904aa4
Tamas Puskas <tamasp1993@live.com>
2019-03-18 17:48:31 +0100

    Done another task

---
+++ orientation/week4/src/main/java/com/greenfoxacademy/resting/controllers/AppendARESTController.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.resting.controllers;
+
+import com.greenfoxacademy.resting.models.AppendA;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppendARESTController {
+
+  @GetMapping("/appenda/{appendable}")
+  public Object appendA(@PathVariable(name = "appendable") String appendable){
+    return new AppendA(appendable);
+  }
+}
--- orientation/week4/src/main/java/com/greenfoxacademy/resting/models/AppendA.java
+++ orientation/week4/src/main/java/com/greenfoxacademy/resting/models/AppendA.java
@@ -1,4 +1,18 @@
 package com.greenfoxacademy.resting.models;
 
 public class AppendA {
+  private String appended;
+
+  public AppendA(String appendable) {
+    this.appended = appendable + "a";
+  }
+
+  public String getAppended() {
+    return appended;
+  }
+
+  public void setAppended(String appended) {
+    this.appended = appended;
+  }
 }
+
